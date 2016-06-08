d4c59b99ce81628f41f6d3b8116866b2d24f7b5e
Adam Schwartz <PhantasyStar.Adam@gmail.com>
2016-06-07 19:46:08 -0700

    price list now generated automatically

--- js/room-script.js
+++ js/room-script.js
@@ -1,4 +1,4 @@
-console.log("room-script.js has run.")
+console.log("room-script.js has run.");
 
 function getQueryVariable(variable)
 {
@@ -17,23 +17,23 @@ console.log(roomValues[roomNumber].name);
 document.querySelector('#pageName').innerHTML = roomValues[roomNumber].name;
 document.querySelector('#pagePrice').innerHTML = roomValues[roomNumber].price;
 
-if (roomValues[roomNumber].king != 0) {
+if (roomValues[roomNumber].king !== 0) {
   document.querySelector('#pageKing').innerHTML = roomValues[roomNumber].king;
 } else {
   document.querySelector('#pageKing').classList.add('hide');
   document.querySelector('#listKing').classList.add('hide');
-};
+}
 
-if (roomValues[roomNumber].queen != 0) {
+if (roomValues[roomNumber].queen !== 0) {
   document.querySelector('#pageQueen').innerHTML = roomValues[roomNumber].queen;
 } else {
   document.querySelector('#pageQueen').classList.add('hide');
   document.querySelector('#listQueen').classList.add('hide');
-};
+}
 
-if (roomValues[roomNumber].twin != 0) {
+if (roomValues[roomNumber].twin !== 0) {
   document.querySelector('#pageTwin').innerHTML = roomValues[roomNumber].twin;
 } else {
   document.querySelector('#pageTwin').classList.add('hide');
   document.querySelector('#listTwin').classList.add('hide');
-};
+}
